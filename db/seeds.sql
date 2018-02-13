a645c44d740d4ed1443d91619a00c389f355ad26
mathewhall100 <mathew.hall@dr.com>
2018-02-13 14:07:50 -0500

    databse seeds.mysql updated

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
+USE trytech_db;
 
-
-INSERT INTO products (product_name, manufacturer, product_description, retail_price, image_url, rating, units_available) 
+INSERT INTO products (product_name, manufacturer, product_description, retail_price, image_url, rating, units_available, createdAt, updatedAt) 
 VALUES
 ("Apple iPhone X, Fully Unlocked 5.8, 64 GB - Silver", 
 "Apple", 
@@ -10,11 +10,11 @@ Wireless Qi charging;
 Splash, water, and dust resistant;
 Sapphire crystal lens cover;",
 1131.96,
-"assets/images/iphone-x.jpeg",
+"/assets/images/iphone-x.jpg",
 8.8,
-5
--- NOW(),
--- NOW()
+5,
+NOW(),
+NOW()
 ),
 
 ("Bose Soundwear Companion Wireless Wearable Speaker - Black",
@@ -25,26 +25,29 @@ Three-button remote lets you make calls, play and pause tracks and access your p
 Plays for up to 12 hours off a single charge with a rechargeable lithium-ion battery;
 Dust, sweat and weather resistant—with an IPX4 rating—and a crinkle-free cover (included with purchase) that won’t cause static;",
 299.00,
-"assets/images/bose-soundware-companion.jpeg",
+"/assets/images/bose-soundwear-companion.jpg",
 8.4,
-4
--- NOW(),
--- NOW()
+4,
+NOW(),
+NOW()
 );
 
+SELECT * FROM products;
 
-INSERT INTO users (user_firstname, user_lastname, user_email, address_street, address_city, address_state, address_zip, payment_method, agreement_signed, subscription_type, date_registered) 
+INSERT INTO users (user_firstname, user_lastname, user_email, address_street, address_city, address_state, address_zip, payment_method, agreement_signed, user_subscription, date_registered, createdAt, updatedAt) 
 VALUES
-("John", "Smith", "jsmith@gmail.com", "4500 Bainbridge Road", "Euclid", "OH", "44236", "credit card", true, 1, "2017-12-02"),				-- NOW(),NOW() 
-("Mel", "Brooks", "mel.brooks@gmail.com", "490 Euclid Ave", "Euclid", "OH", "44236", "credit card", true, 1, "2017-12-24"), 				-- NOW(),NOW()
-("Bill", "Pettiti", "pettiti250h@gmail.com", "23500 Pettibone Road", "Bainbridge", "OH", "44100", "paypal", true, 2, "2018-01-05"), 		-- NOW(),NOW()
-("Suzie", "Banshee", "thebanshee@outlook.com", "200 Hollywood Blvd", "Beverley Hills", "CA", "90210", "voucher", true, 1, "2018-02-02"); 	-- NOW(),NOW()
+("John", "Smith", "jsmith@gmail.com", "4500 Bainbridge Road", "Euclid", "OH", "44236", "credit card", true, 1, "2017-12-02", NOW(), NOW()),			
+("Mel", "Brooks", "mel.brooks@gmail.com", "490 Euclid Ave", "Euclid", "OH", "44236", "credit card", true, 1, "2017-12-24", NOW(), NOW()), 				
+("Bill", "Pettiti", "pettiti250h@gmail.com", "23500 Pettibone Road", "Bainbridge", "OH", "44100", "paypal", true, 2, "2018-01-05", NOW(), NOW()), 		
+("Suzie", "Banshee", "thebanshee@outlook.com", "200 Hollywood Blvd", "Beverley Hills", "CA", "90210", "voucher", true, 1, "2018-02-02", NOW(),NOW());	
 
+SELECT * FROM users;
 
-INSERT INTO subscriptions (tier_name, monthly_cost, num_devices, num_days)
+INSERT INTO subscriptions (tier_name, monthly_cost, num_devices, num_days, createdAt, updatedAt)
 VALUES
-("pay as you go", 00.00, 1, 5),  -- NOW(),NOW() 
-("basic", 9.99, 3, 5),          -- NOW(),NOW() 
-("gold", 14.99, 3, 7),          -- NOW(),NOW() 
-("protester", 29.99, 10, 7);    -- NOW(),NOW() 
+("pay as you go", 00.00, 1, 5, NOW(),NOW()),
+("basic", 9.99, 3, 5, NOW(),NOW()),
+("gold", 14.99, 3, 7, NOW(),NOW()),
+("protester", 29.99, 10, 7, NOW(),NOW());
 
+SELECT * FROM subscriptions;
