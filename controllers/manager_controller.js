4ca95cdf955486bf3bc43db48390a60d0721fb71
mathewhall100 <mathew.hall@dr.com>
2018-02-18 12:30:30 -0500

    manager routes and handlebars page added

--- controllers/manager_controller.js
+++ controllers/manager_controller.js
@@ -9,11 +9,52 @@ var db = require("../models");
 // create routes with logic as required
 // ===================================
 
+// Route to display manager portal
+
+routerManager.get("/manager", function (req, res) {
+
+    res.render("manager");
+});
 
 
 
+routerManager.get("/manager/:action", function (req, res) {
 
+    var action = req.params.action;
 
+    switch (action) {
+
+        case "1":
+            res.render("index");
+
+            break;
+
+        case 2:
+
+        console.log("2")
+
+            break;
+
+        default:
+
+    }
+
+    // db.Product.findAll({
+    //     order: [
+    //         ['product_name', 'ASC']
+    //     ],
+    //     where: {
+    //         category_id: req.params.id
+    //     }
+    // }).then(function (productList) {
+
+    //     var hbsObj = {
+    //         products: productList
+    //     };
+
+    // res.render("categories", hbsObj);
+    // });
+});
 
 
 
